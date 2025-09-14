96aadb0e402058a38e72a3bb67d6a4380348904c
foxintango@allinone.llama <foxintango@allinone.llama>
2025-09-15 07:35:32 +0800

    Mon Sep 15 07:35:32 AM CST 2025

--- .make/depend.dir/libui.mk
+++ .make/depend.dir/libui.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libui.recursive
 ECHO_TARGETS += libui.echo
 libui.recursive:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libtango/               >> /home/lidali/alpine/libraries/libui/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libtango/.make >> /home/lidali/alpine/libraries/libui/.make/super
-	cd /home/lidali/alpine/libraries/libui/ && make recursive && make install
-	-rm /home/lidali/alpine/libraries/libui/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libtango/               >> /volumes/llama/home/alpine/libraries/libui/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libtango/.make >> /volumes/llama/home/alpine/libraries/libui/.make/super
+	cd /volumes/llama/home/alpine/libraries/libui/ && make recursive && make install
+	-rm /volumes/llama/home/alpine/libraries/libui/.make/super
 libui.echo:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libtango/               >> /home/lidali/alpine/libraries/libui/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libtango/.make >> /home/lidali/alpine/libraries/libui/.make/super
-	cd /home/lidali/alpine/libraries/libui/ && make echo
-	-rm /home/lidali/alpine/libraries/libui/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libtango/               >> /volumes/llama/home/alpine/libraries/libui/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libtango/.make >> /volumes/llama/home/alpine/libraries/libui/.make/super
+	cd /volumes/llama/home/alpine/libraries/libui/ && make echo
+	-rm /volumes/llama/home/alpine/libraries/libui/.make/super
