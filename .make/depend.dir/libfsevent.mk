ea3f0d855e91c5541ad8e4db568dc97d1f1f2812
foxintango@allinone.llama <foxintango@allinone.llama>
2025-09-19 00:40:45 +0800

    Fri Sep 19 12:40:45 AM CST 2025

---
+++ .make/depend.dir/libfsevent.mk
@@ -0,0 +1,12 @@
+DEPEND_TARGETS += libfsevent.recursive
+ECHO_TARGETS += libfsevent.echo
+libfsevent.recursive:
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libtango/               >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libtango/.make >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	cd /volumes/llama/home/alpine/libraries/libfsevent/ && make recursive && make install
+	-rm /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+libfsevent.echo:
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libtango/               >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libtango/.make >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	cd /volumes/llama/home/alpine/libraries/libfsevent/ && make echo
+	-rm /volumes/llama/home/alpine/libraries/libfsevent/.make/super
